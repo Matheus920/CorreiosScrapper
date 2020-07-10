1663a4e5408f0d3eb43d4f798a5054b99edacca9
Matheus Moreira <matheusm920@gmail.com>
2020-07-09 22:37:46 -0300

    Minor adjusts in usability

--- cli.js
+++ cli.js
@@ -12,7 +12,7 @@ require('yargs')
             describe: 'The object you want to track'
         });
     }, function (argv) {
-        if (!argv.object_code.match('[A-Z]{2}[0-9]{9}[A-Z]{2}')) {
+        if (!argv.object_code || !argv.object_code.match('[A-Z]{2}[0-9]{9}[A-Z]{2}')) {
             console.log('Please enter a valid brazilian object code format');
         } else {
             var object_code = argv.object_code;
@@ -24,7 +24,6 @@ require('yargs')
                     args: ['--no-sandbox',
                         '--disable-setuid-sandbox',
                         '--disable-infobars',
-                        '--window-position=0,0',
                         '--ignore-certificate-errors',
                         '--ignore-certificate-errors-spki-list',
                         '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"']
@@ -53,5 +52,6 @@ require('yargs')
             })();
         }
     })
+    .demandCommand()
     .help()
     .argv
